d3066e94935d48c527a5cb51f770f73ce765229b
Sroussi Nathan <nathansroussi@gmail.com>
2022-11-02 18:22:27 +0100

    Init_Project

---
+++ includ/fonction.php
@@ -0,0 +1,14 @@
+<?php
+function connecterServeurBD() 
+{
+    $PARAM_hote='localhost'; 
+    $PARAM_port='3306';
+    $PARAM_nom_bd=''; // le nom de votre base de donn�es
+    $PARAM_utilisateur='root'; // nom d'utilisateur pour se connecter
+    // mot de passe de l'utilisateur pour se connecter
+
+    $connect = new PDO('mysql:host='.$PARAM_hote.';port='.$PARAM_port.';dbname='.$PARAM_nom_bd, $PARAM_utilisateur,'root');
+ 
+    return $connect;
+}
+?>
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+include('includ/fonction.php');
+$a = connecterServeurBD();
+var_dump($a);
+
+?>
