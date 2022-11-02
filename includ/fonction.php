69cc0df4c6dfb27e07db585ff515e78a03165eea
Sroussi Nathan <nathansroussi@gmail.com>
2022-11-02 20:15:43 +0100

    test bdd

--- includ/fonction.php
+++ includ/fonction.php
@@ -3,7 +3,7 @@ function connecterServeurBD()
 {
     $PARAM_hote='localhost'; 
     $PARAM_port='3306';
-    $PARAM_nom_bd=''; // le nom de votre base de donn�es
+    $PARAM_nom_bd='test'; // le nom de votre base de donn�es
     $PARAM_utilisateur='root'; // nom d'utilisateur pour se connecter
     // mot de passe de l'utilisateur pour se connecter
 
